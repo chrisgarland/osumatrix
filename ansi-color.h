e816509560a3b0b1d088faa451f1150fede296ed
chrisgarland <1chris.garland1@gmail.com>
2016-04-19 00:03:19 +0800

    ansi colour coding

---
+++ ansi-color.h
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  ansi-color.h
+ *
+ *    Description:  Header file containing ansi colour constants
+ *
+ *        Version:  1.0
+ *        Created:  16/04/2016 23:38:37
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  CHRIS GARLAND (), c.garland@student.curtin.edu.au
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#define KNRM  "\x1B[0m"   //End colour tag: Necessary 
+
+#define KRED  "\x1B[31m"  //Red 
+#define KGRN  "\x1B[32m"  //Green 
+#define KYEL  "\x1B[33m"  //Yellow 
+#define KBLU  "\x1B[34m"  //Blue
+#define KMAG  "\x1B[35m"  //Magenta
+#define KCYN  "\x1B[36m"  //CYN?
+#define KWHT  "\x1B[37m"  //White
+
