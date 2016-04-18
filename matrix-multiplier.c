53faa54c570b26b163d954b8fbe416a220022955
chrisgarland <1chris.garland1@gmail.com>
2016-04-19 00:49:38 +0800

    Initial wrapper finished

--- matrix-multiplier.c
+++ matrix-multiplier.c
@@ -40,7 +40,9 @@ int main ( int argc, char *argv[] )
                 FILE* matrix_description_file = (argv[1], "r"); 
                 int **matrix_A = build_matrix(matrix_description_file); 
                 int **matrix_B = build_matrix(matrix_description_file); 
-                int **
+                int **matrix_C = multiply_matricies(matrix_A, matrix_B);
+                
+                os200_print_matrix(matrix_C);
         }
         return EXIT_SUCCESS;
 }		/* ----------  end of function main  ---------- */
