ff3bfe0d31eef2cd5c099592c1a5b13f60afb39a
chrisgarland <1chris.garland1@gmail.com>
2016-04-19 12:21:28 +0800

    Moved truncate_file() to util.h

---
+++ matrix-builder.c
@@ -0,0 +1,40 @@
+/*
+ * =============================================================================
+ *
+ *       Filename:  matrix-builder.c
+ *
+ *    Description:  Inplemtation of a matrix builder using a 2D array
+ *
+ *        Version:  1.0
+ *        Created:  19/04/2016 10:29:54
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  CHRIS GARLAND (), c.garland@student.curtin.edu.au
+ *   Organization:  
+ *
+ * =============================================================================
+ */
+#include "util.h"
+
+#include <stdlib.h>
+
+/* 
+ * ===  FUNCTION  ==============================================================
+ *         Name:  build_matrix
+ *  Description:  
+ * =============================================================================
+ */
+char** build_matrix (FILE* _input_file)
+{
+        char *line_buffer;
+        char *word_buffer;
+        int num_rows;
+        int num_colums;
+
+        truncate_file(_input_file, 2);   /*cuts the first 2 lines off the file*/ 
+
+
+        return <+return value+>;
+}	/* -----  end of function build_matrix  ----- */
+
