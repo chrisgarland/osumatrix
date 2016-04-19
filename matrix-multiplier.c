c11d7578b857e0ae286e8129f5a24ee094182265
chrisgarland <1chris.garland1@gmail.com>
2016-04-19 12:22:23 +0800

    Added test macro

--- matrix-multiplier.c
+++ matrix-multiplier.c
@@ -1,9 +1,9 @@
 /*
- * =====================================================================================
+ * =============================================================================
  *
  *       Filename:  matrix-multiplier.c
  *
- *    Description:  Implementation of a matrix multiplier
+ *    Description:  Implementation of a matrix multiplier. Contains main()
  *
  *        Version:  1.0
  *        Created:  18/04/2016 21:17:05
@@ -13,27 +13,29 @@
  *         Author:  CHRIS GARLAND (15560955), c.garland@student.curtin.edu.au
  *   Organization:  
  *
- * =====================================================================================
+ * ============================================================================
  */
-#include "ansi-color.h"    /*Ansi colour coding*/ 
+#include "ansi-color.h"    /*Ansi colour coding used for output to sdtout*/ 
 
 #include <stdlib.h>
 #include <stdio.h>
 
+#define TEST_PRINT
+
 /* 
- * ===  FUNCTION  ======================================================================
+ * ===  FUNCTION  ============================================================
  *         Name:  main
- *  Description:  Wrapper for matrix multiplier. Takes a description file, builds the 
- *                data structures & multiplies the matrices
+ *  Description:  Wrapper for matrix multiplier. Takes a description file, 
+ *                builds the data structures & multiplies the matrices
  *
  *        Input:  Matrix description file
  *       Output:  Product of two matrices
- * =====================================================================================
+ * =============================================================================
  */
 int main ( int argc, char *argv[] )
 {
         if (argc != 2) {
-                ferror(KWHT"\nMust enter an argument: Input file\n\n");
+                perror(KWHT"\nMust enter an argument: Input file\n\n");
                 printf(KNRM);
         } else {
                 FILE* matrix_description_file = (argv[1], "r"); 
@@ -41,7 +43,11 @@ int main ( int argc, char *argv[] )
                 int **matrix_B = build_matrix(matrix_description_file); 
                 int **matrix_C = multiply_matricies(matrix_A, matrix_B);
                 
-                os200_print_matrix(matrix_C);
+                #ifdef TEST_PRINT 
+                        os200_print_matrix(matrix_A);
+                        os200_print_matrix(matrix_B);
+                        os200_print_matrix(matrix_C);
+                #endif
         }
         return EXIT_SUCCESS;
 }		/* ----------  end of function main  ---------- */
