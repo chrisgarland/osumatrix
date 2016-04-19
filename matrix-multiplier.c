08ade10a9ff34f7a68e1da3b100f5e1d81392cb8
chrisgarland <1chris.garland1@gmail.com>
2016-04-19 15:58:19 +0800

    Preprocessor testing

--- matrix-multiplier.c
+++ matrix-multiplier.c
@@ -16,6 +16,7 @@
  * ============================================================================
  */
 #include "ansi-color.h"    /*Ansi colour coding used for output to sdtout*/ 
+#include "matrix.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -39,9 +40,9 @@ int main ( int argc, char *argv[] )
                 printf(KNRM);
         } else {
                 FILE* matrix_description_file = (argv[1], "r"); 
-                int **matrix_A = build_matrix(matrix_description_file); 
-                int **matrix_B = build_matrix(matrix_description_file); 
-                int **matrix_C = multiply_matricies(matrix_A, matrix_B);
+                Matrix *matrix_A = build_matrix(matrix_description_file); 
+                Matrix *matrix_B = build_matrix(matrix_description_file); 
+                Matrix *matrix_C = multiply_matricies(matrix_A, matrix_B);
                 
                 #ifdef TEST_PRINT 
                         os200_print_matrix(matrix_A);
